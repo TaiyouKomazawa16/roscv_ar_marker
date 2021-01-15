b0c91e2450854e203a32c6a1b01ea74efbbae14c
Taiyou Komazawa <e1611216@toyama.kosen-ac.jp>
2021-01-15 20:15:26 +0900

    姿勢をオイラー角に統一

--- src/charuco_ros.cpp
+++ src/charuco_ros.cpp
@@ -29,61 +29,33 @@ const int target_id = 6;
 std::string ar_frame = "ar_mark_" + std::to_string(target_id);
 std::string ar_frame_depth = "ar_mark_" + std::to_string(target_id) + "_depth";
 
-tf2::Quaternion getQuaternion(cv::Vec3d rvec) {
+cv::Vec3d getEuler(cv::Vec3d rvec)
+{
     // 回転ベクトルrvecを3x3の回転行列Rに変換
     cv::Mat R;
     Rodrigues(rvec, R);
+    float sy = sqrt(R.at<double>(0, 0) * R.at<double>(0, 0) + R.at<double>(1, 0) * R.at<double>(1, 0));
+    bool singular = sy < 1e-6; // If
 
-    // Mat型配列の要素をMatrix3d型配列に格納
-    Eigen::Matrix3d eigen_R;
-    for(int i = 0; i < 3; i++) {
-        for(int j = 0; j < 3; j++) {
-            eigen_R(i, j) = R.at<double>(i, j);
-        }
+    float x, y, z;
+    if(!singular){
+        x = atan2(R.at<double>(2, 1), R.at<double>(2, 2));
+        y = atan2(-R.at<double>(2, 0), sy);
+        z = atan2(R.at<double>(1, 0), R.at<double>(0, 0));
+    }else{
+        x = atan2(-R.at<double>(1, 2), R.at<double>(1, 1));
+        y = atan2(-R.at<double>(2, 0), sy);
+        z = 0;
     }
+    cv::Vec3d ea_;
+    ea_[0] = -z;
+    ea_[2] = y;
+    ea_[1] = x;
 
-    // 回転行列Rを四元数Qに変換
-    Eigen::Quaterniond Q(eigen_R);
-    tf2::Quaternion q_tf(Q.z(),-Q.x(),-Q.y(),Q.w());
-
-    return q_tf;
+    return ea_;
 }
 
-void ar_broadcast(ros::Publisher &pub, std::string ar_frame, Vec3d tvec, Vec3d rvec)
-{
-    static tf2_ros::TransformBroadcaster tfb;
-    geometry_msgs::TransformStamped transformStamped;
-    geometry_msgs::PoseWithCovarianceStamped msg;
-
-    transformStamped.header.frame_id = "camera_color_frame";
-    msg.header.frame_id = "camera_color_frame";
-    transformStamped.child_frame_id = ar_frame.c_str();
-    transformStamped.transform.translation.x = tvec[2];
-    transformStamped.transform.translation.y = -tvec[0];
-    transformStamped.transform.translation.z = -tvec[1];
-    tf2::Quaternion q;
-    q = getQuaternion(rvec);
-    transformStamped.transform.rotation.x = q.x();
-    transformStamped.transform.rotation.y = q.y();
-    transformStamped.transform.rotation.z = q.z();
-    transformStamped.transform.rotation.w = q.w();
-
-    msg.pose.pose.position.x = transformStamped.transform.translation.x;
-    msg.pose.pose.position.y = transformStamped.transform.translation.y;
-    msg.pose.pose.position.z = transformStamped.transform.translation.z;
-    msg.pose.pose.orientation = transformStamped.transform.rotation;
-
-    //ROS_INFO("marker T:[%f,%f,%f]\tR:[%f,%f,%f,%f]",
-    //         tvec[0], tvec[1], tvec[2],
-    //         q.x(), q.y(), q.z(), q.w());
-
-    msg.header.stamp = ros::Time::now();
-    pub.publish(msg);
-    transformStamped.header.stamp = ros::Time::now();
-    tfb.sendTransform(transformStamped);
-}
-
-void ar_broadcast_rpy(ros::Publisher &pub, std::string ar_frame, Vec3d tvec, Vec3d rpy)
+void ar_broadcast(ros::Publisher &pub, std::string ar_frame, Vec3d tvec, Vec3d rpy)
 {
     static tf2_ros::TransformBroadcaster tfb;
     geometry_msgs::TransformStamped transformStamped;
@@ -125,9 +97,7 @@ int main(int argc, char **argv)
     RosImageConverter rosdepth("/camera/aligned_depth_to_color/image_raw", "/camera/aligned_depth_to_color/camera_info",
                                     sensor_msgs::image_encodings::TYPE_16UC1);
     ros::NodeHandle nh;
-    image_transport::ImageTransport it(nh);
-    image_transport::Publisher img_pub;
-    img_pub = it.advertise("/charuco_tracker/result", 1);
+
     ros::Publisher rgb_pub = nh.advertise<geometry_msgs::PoseWithCovarianceStamped>
                                     (ar_frame + "/rgb_pose", 10);
     ros::Publisher depth_pub = nh.advertise<geometry_msgs::PoseWithCovarianceStamped>
@@ -162,7 +132,7 @@ int main(int argc, char **argv)
     vector<cv::Point2f> charucoCorners;
     vector<cv::Vec3d> rvecs, tvecs;
 
-    cv::Vec3d rgb_rvec, rgb_tvec;
+    cv::Vec3d rgb_rpy, rgb_tvec;
     cv::Vec3d depth_rpy, depth_tvec;
 
     ros::Rate rate(30);
@@ -200,7 +170,7 @@ int main(int argc, char **argv)
                             vector<cv::Point2f> dcor = diamondCorners[i];
 
                             rgb_tvec = tvecs[i];
-                            rgb_rvec = rvecs[i];
+                            rgb_rpy = getEuler(rvecs[i]);
 
                             cv::Point pt[4] = {dcor[0], dcor[1], dcor[2], dcor[3]};
                             /*マーカの重心座標の検出*/
@@ -233,15 +203,15 @@ int main(int argc, char **argv)
                             cv::Mat U = cv::Mat::zeros(3,1, CV_64F);
                             cv::Mat V = cv::Mat::zeros(3,1, CV_64F);
 
-                            double pt_c_x = (ptc.x - min_pt.x);
-                            double pt_c_y = (ptc.y - min_pt.y);
+                            double pt_c_x = (ptc.x - min_pt.x); //マーカー領域の中心位置x[px]
+                            double pt_c_y = (ptc.y - min_pt.y); //マーカー領域の中心位置y[px]
 
                             for(int i = 0; i < sz.height; i++){ //画像y方向
                                 for(int j = 0; j < sz.width; j++){ //画像x方向
-                                    ushort depth = dmarker.at<ushort>(j, i);
+                                    ushort depth = dmarker.at<ushort>(j, i); //z[mm]
                                     if(depth != 0){
-                                        double _i = (double)i - pt_c_x;
-                                        double _j = (double)j - pt_c_y;
+                                        double _i = (double)(i - pt_c_x) / depthMatrix.at<double>(0, 0) * depth; //y[mm]
+                                        double _j = (double)(j - pt_c_y) / depthMatrix.at<double>(1, 1) * depth; //x[mm]
                                         M.at<double>(0,0) += 1.0;
                                         M.at<double>(1,1) += _j*_j;
                                         M.at<double>(2,2) += _i*_i;
@@ -259,7 +229,7 @@ int main(int argc, char **argv)
                             }
 
                             cv::solve(M, U, V, DECOMP_LU); //LU分解にて平面の定数行列(V)を導出
-                            //平面の式: z = V1 * x + V2 * y + V0
+                            //平面の式: z[mm] = V1 * x[mm] + V2 * y[mm] + V0
 
                             //マーカ中心部の推定距離を測定
                             double z_c = V.at<double>(0, 0);
@@ -267,27 +237,17 @@ int main(int argc, char **argv)
                             double x_c = ((ptc.x - depthMatrix.at<double>(0, 2)) / depthMatrix.at<double>(0, 0)) * z_c;
                             double y_c = ((ptc.y - depthMatrix.at<double>(1, 2)) / depthMatrix.at<double>(1, 1)) * z_c;
 
-                            double pt_1_x = (pt[1].x - ptc.x);
-                            double pt_1_y = (pt[1].y - ptc.y);
-                            double z_1 = V.at<double>(0, 0) + V.at<double>(1, 0) * pt_1_x + V.at<double>(2, 0) * pt_1_y;
-                            double x_1 = ((pt[1].x - depthMatrix.at<double>(0, 2)) / depthMatrix.at<double>(0, 0)) * z_1;
-                            double y_1 = ((pt[1].y - depthMatrix.at<double>(1, 2)) / depthMatrix.at<double>(1, 1)) * z_1;
-
-
-                            //ROS_INFO("depth:[%lf,%lf,%lf]", x_c/1E3, y_c/1E3, z_c/1E3);
-
-                            double x_1c = x_1 - x_c;
-                            double y_1c = y_1 - y_c;
-                            double z_1c = z_c - z_1;
-
-                            cv::Vec3d rvec, tvec;
+                            cv::Vec3d rpy, tvec;
                             tvec[0] = x_c / 1E3;
                             tvec[1] = y_c / 1E3;
                             tvec[2] = z_c / 1E3;
 
-                            // V1 * x + V2 * y -1 * z + V0 = 0
-                            double rxy = std::atan2(pt_1_y / sz.height, pt_1_x / sz.width) + M_PI / 4.0;
+                            double pt_1c_x = pt[1].x - ptc.x; //中心と45度方向のマーカー角1との位置ベクトルx[px]
+                            double pt_1c_y = pt[1].y - ptc.y; //中心と45度方向のマーカー角1との位置ベクトルy[px]
 
+                            double rxy = std::atan2(pt_1c_y / sz.height, pt_1c_x / sz.width) + M_PI / 4.0;
+
+                            // V1*x + V2*y - 1*z + V0 = 0
                             cv::Mat NV = cv::Mat::zeros(3, 1, CV_64F); //法線ベクトル
                             NV.at<double>(0) = V.at<double>(1, 0);
                             NV.at<double>(1) = V.at<double>(2, 0);
@@ -296,25 +256,13 @@ int main(int argc, char **argv)
                             double roll = atan2(NV.at<double>(2), NV.at<double>(0)) + M_PI/2;
                             double pitch = atan2(NV.at<double>(2), NV.at<double>(1)) - M_PI/2;
                             double yaw = -rxy - M_PI;
-                            rvec[1] = roll;
-                            rvec[2] = pitch;
-                            rvec[0] = yaw;
+                            rpy[1] = roll;
+                            rpy[2] = pitch;
+                            rpy[0] = yaw;
 
                             depth_tvec = tvec;
-                            depth_rpy = rvec;
+                            depth_rpy = rpy;
 
-                            cv::Mat flat = cv::Mat::zeros(50, 50, CV_16UC1);
-
-                            for(int i = 0; i < 50; i++){
-                                for(int j = 0; j < 50; j++){
-                                    double depth = V.at<double>(0, 0) + V.at<double>(1, 0) * sz.width * (j / 50.0) + V.at<double>(2, 0) * sz.height * (i / 50.0);
-                                    if(depth < 0){ depth = 0; }
-                                    flat.at<ushort>(j,i) = depth;
-                                }
-                            }
-
-                            sensor_msgs::ImagePtr msg = cv_bridge::CvImage(std_msgs::Header(), "16UC1", flat).toImageMsg();
-                            img_pub.publish(msg);
 
                         }
                     }
@@ -323,8 +271,8 @@ int main(int argc, char **argv)
 
             //cv::imshow("Aruco", imageCopy); //結果をウィンドウ(Aruco)に表示
         }
-        ar_broadcast(rgb_pub, ar_frame, rgb_tvec, rgb_rvec);
-        ar_broadcast_rpy(depth_pub, ar_frame_depth, depth_tvec, depth_rpy);
+        ar_broadcast(rgb_pub, ar_frame, rgb_tvec, rgb_rpy);
+        ar_broadcast(depth_pub, ar_frame_depth, depth_tvec, depth_rpy);
 
         ros::spinOnce();
         rate.sleep();
